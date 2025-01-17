281526c69855487670ef20e0cb0746ba84f30c6b
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2025-01-17 10:33:56 +0200

    commit

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ProductFactory extends Factory
 {
+    // protected $model = Product::class;
     /**
      * Define the model's default state.
      *
@@ -17,12 +18,12 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         $this->faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($this->faker));
+        
         return [
-            'name'=>$this->faker->productName,
-            'price'=>$this->faker->randomFloat(4, 10, 1000),
-            'description'=>$this->faker->text,
-            'image'=>'https://picsum.photos/seed/picsum/200/300'
-    
+            'name' => $this->faker->productName,
+            'price' => $this->faker->randomFloat(4, 10, 1000),
+            'description' => $this->faker->text,
+            'image' => 'https://picsum.photos/seed/'.$this->faker->text(12).'/200/300',
         ];
     }
 }
