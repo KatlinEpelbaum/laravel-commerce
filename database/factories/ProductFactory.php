54075c6ab7c4d4d358a53f20969e1216284b1241
EmilySelge <selge.emily@gmail.com>
2025-01-17 09:22:23 +0200

    first commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $this->faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($this->faker));
+        return [
+            'name'=>$this->faker->productName,
+            'price'=>$this->faker->randomFloat(4, 10, 1000),
+            'description'=>$this->faker->text,
+            'image'=>'https://picsum.photos/seed/picsum/200/300'
+    
+        ];
+    }
+}
---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,57 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                <section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900">
+            {{ __('Profile Information') }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600">
+            {{ __("Update your account's profile information and email address.") }}
+        </p>
+    </header>
+
+    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+        @csrf
+    </form>
+
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+        @csrf
+        @method('patch')
+
+        <div>
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" 
+                :value="old('name', $product->name)" required autofocus autocomplete="name" />
+            <x-input-error class="mt-2" :messages="$errors->get('name')" />
+        </div>
+
+        <div>
+            <x-input-label for="price" :value="__('Price')" />
+            <x-text-input id="price" name="price" type="price" class="mt-1 block w-full" 
+            :value="old('price', $product->price)" required autocomplete="price" />
+            <x-input-error class="mt-2" :messages="$errors->get('price')" />
+        </div>
+
+        <div>
+            <x-input-label for="image" :value="__('Image')" />
+            <x-text-input id="image" name="image" type="image" class="mt-1 block w-full" 
+            :value="old('image', $product->image)" required autocomplete="" />
+            <x-input-error class="mt-2" :messages="$errors->get('image')" />
+        </div>
+    </form>
+</section>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
